ac3933c2dc11fd919065f56e12e3e5a804d8e230
Swagatika Behera <swagatikabehera2001@gmail.com>
2021-07-24 10:33:06 +0530

    setting react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Route } from "react-router-dom";
+
+function Root() {
+  return <h1>hello root</h1>;
+}
+
+function Movie() {
+  return <h1>hello movie</h1>;
+}
+
 function App() {
   return (
-    <div className="App p-10">
-      <button className="border-4 px-2 py-2 border-blue-500 md:border-green-500  rounded bg-runu-600 text-white hover:bg-transparent hover:text-blue-500 hover:border-green-500 hover:border-8 focus:ring focus:border-blue-300">
-        tailwind
-      </button>
-    </div>
+    <>
+      <Route path="/" exact component={Root} />
+      <Route path="/movie" exact component={Movie} />
+    </>
   );
 }
 
