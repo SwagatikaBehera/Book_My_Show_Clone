f0a993c705adde16e4dcdfec25363b13a4a58b8a
Swagatika Behera <swagatikabehera2001@gmail.com>
2021-10-16 10:14:32 +0530

    fix play page error

--- src/pages/Plays.page.jsx
+++ src/pages/Plays.page.jsx
@@ -14,29 +14,7 @@ const Plays = () => {
             <div className="flex flex-wrap">
               <div className="w-1/2 md:w-1/3 my-4 lg:w-3/12">
                 <Poster
-                  src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNCBBdWcgb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:oi-discovery-catalog@@icons@@bundle-icon-shadow-4x.png,ox-15,oy-15,ow-50/et00313464-azyxnpqxnd-portrait.jpg"
-                  title="Digital Theatre Fest By EnActe, Rage & Bookmyshow"
-                  subtitle="Multiple Venues"
-                />
-              </div>
-              <div className="w-1/2 md:w-1/3 my-4 lg:w-3/12">
-                <Poster
-                  src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNCBBdWcgb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:oi-discovery-catalog@@icons@@bundle-icon-shadow-4x.png,ox-15,oy-15,ow-50/et00313464-azyxnpqxnd-portrait.jpg"
-                  title="Digital Theatre Fest By EnActe, Rage & Bookmyshow"
-                  subtitle="Multiple Venues"
-                />
-              </div>
-              <div className="w-1/2 md:w-1/3 my-4 lg:w-3/12">
-                <Poster
-                  src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNCBBdWcgb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:oi-discovery-catalog@@icons@@bundle-icon-shadow-4x.png,ox-15,oy-15,ow-50/et00313464-azyxnpqxnd-portrait.jpg"
-                  title="Digital Theatre Fest By EnActe, Rage & Bookmyshow"
-                  subtitle="Multiple Venues"
-                />
-              </div>
-              <div className="w-1/2 md:w-1/3 my-4 lg:w-3/12">
-                <Poster
-                  src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNCBBdWcgb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:oi-discovery-catalog@@icons@@bundle-icon-shadow-4x.png,ox-15,oy-15,ow-50/et00313464-azyxnpqxnd-portrait.jpg"
-                  title="Digital Theatre Fest By EnActe, Rage & Bookmyshow"
+                  original_title="Digital Theatre Fest By EnActe, Rage & Bookmyshow"
                   subtitle="Multiple Venues"
                 />
               </div>
@@ -84,7 +62,7 @@ const Plays = () => {
             <div>
               <PlaysFilters
                 title="Price"
-                tags={["Free", "0-500", "501-2000","Above 2000"]}
+                tags={["Free", "0-500", "501-2000", "Above 2000"]}
               />
             </div>
           </div>
